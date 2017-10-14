83a4a7c5c589acb23e2b956fbd6a54804734b287
DrYueh <mameron2255@gmail.com>
2017-10-13 21:41:32 -0600

    removed doors from elevator

--- Assets/Scripts/elevator.cs
+++ Assets/Scripts/elevator.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class elevator : MonoBehaviour {
+public class Elevator : MonoBehaviour {
 	public int startPositionX;
 	public int startPositionY;
 	public int numNodesInElevator;
 	private int[] position;
 	private Node[] nodes;
-	private Door left;
-	private Door right;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +16,7 @@ public class elevator : MonoBehaviour {
 		position [1] = startPositionY;
 
 		nodes = new Node[numNodesInElevator];
-		left = new Door ();
-		right = new Door ();
-		
+
 	}
 	
 	// Update is called once per frame
