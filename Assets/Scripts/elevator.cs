144bef291cd913f6215fb1017374709088ad25c7
DrYueh <mameron2255@gmail.com>
2017-10-13 22:39:44 -0600

    added door things to elevator

--- Assets/Scripts/elevator.cs
+++ Assets/Scripts/elevator.cs
@@ -2,12 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Elevator : MonoBehaviour {
 	public int startPositionX;
 	public int startPositionY;
 	public int numNodesInElevator;
+
+	private Floor floor;
 	private int[] position;
 	private Node[] nodes;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.GetComponent<Floor>() != null) {
+			floor = other.GetComponent<Floor> ();
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if other.GetComponent<Floor>() == floor){
+			floor = null;
+		}
+	}
+	void ConnectNode(){
+		
+	}
+	void DisConnectNode(){
+
+	}
+
+	void CloseLeftDoor(){
+		floor.GetDoorLeft ().Close ();
+
+	}
+	void OpenLeftDoor(){
+		floor.GetDoorLeft ().Open ();
+	}
+	void CloseRightDoor(){
+		floor.GetDoorRight ().Close ();
+	}
+	void OpenRightDoor(){
+		floor.GetDoorRight ().Open ();
+	}
+
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +53,25 @@ public class Elevator : MonoBehaviour {
 		nodes = new Node[numNodesInElevator];
 
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown (0)) {
+			if (floor.GetDoorLeft ().open) {
+				CloseLeftDoor ();
+			} else {
+				OpenLeftDoor ();
+			}
+		}
+		else if (Input.GetMouseButtonDown(1)){
+			if (floor.GetDoorRight ().open) {
+				CloseRightDoor ();
+			} else {
+				OpenRightDoor ();
+			}
+		}
+
 	}
 }
