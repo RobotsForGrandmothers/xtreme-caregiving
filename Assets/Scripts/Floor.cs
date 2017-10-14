da3835e9302eac84650a0fb71a2b1824b1e03f8e
asampley <sampley@ualberta.ca>
2017-10-13 22:38:36 -0600

    Added doors to floors

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Floor : MonoBehaviour {
+	public GameObject doorPrefab;
+
 	public int halfLength;
 	public float nodeSpacing = 1;
 	public float middleWidth = 1;
 	Node[] nodes;
+	Door left;
+	Door right;
 
 	// Use this for initialization
 	void Start () {
+		left = Instantiate (doorPrefab, this.transform).GetComponent<Door>();
+		left.transform.localPosition = new Vector2(-middleWidth / 2, 0);
+		right = Instantiate (doorPrefab, this.transform).GetComponent<Door>();
+		right.transform.localPosition = new Vector2(+middleWidth / 2, 0);
+
 		nodes = new Node[halfLength * 2];
 
 		for (int i = 0; i < halfLength; ++i) {
 			nodes [i] = CreateNode(i);
 			nodes [i + halfLength] = CreateNode (i + halfLength);
-			nodes [i].transform.transform.localPosition = new Vector2 (-(middleWidth / 2 + i * nodeSpacing), 0);
-			nodes [i + halfLength].transform.transform.localPosition = new Vector2 (+(middleWidth / 2 + i * nodeSpacing), 0);
+			nodes [i].transform.transform.localPosition = new Vector2 (-(middleWidth / 2 + (i + 1) * nodeSpacing), 0);
+			nodes [i + halfLength].transform.transform.localPosition = new Vector2 (+(middleWidth / 2 + (i + 1) * nodeSpacing), 0);
 
 			if (i > 0) {
 				nodes [i].right = nodes [i - 1];
