4b99cf5251ed651c1cba92586364bcdafd036692
asampley <sampley@ualberta.ca>
2017-10-13 21:14:55 -0600

    Created Floor prefab and Node script

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+	public int length;
+	public float nodeSpacing;
+	Node[] nodes;
+
+	// Use this for initialization
+	void Start () {
+		nodes = new Node[length];
+
+		for (int i = 0; i < length; ++i) {
+			GameObject node = new GameObject("Node " + i);
+			node.transform.SetParent (this.transform);
+			node.AddComponent<Node>();
+			nodes [i] = node.GetComponent<Node>();
+			nodes [i].transform.transform.localPosition = new Vector2 (i * nodeSpacing, 0);
+		}
+	}
+}
