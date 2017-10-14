4c1029305e0fd1ec8635cbd42d0015260284fcaa
DrYueh <mameron2255@gmail.com>
2017-10-13 21:11:22 -0600

    added elevator script

---
+++ Assets/Scripts/elevator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator : MonoBehaviour {
+	public int startPositionX;
+	public int startPositionY;
+	public int numNodesInElevator;
+	private int[] position;
+	private Node[] nodes;
+	private Door left;
+	private Door right;
+
+	// Use this for initialization
+	void Start () {
+		position = new int[2];
+		position [0] = startPositionX;
+		position [1] = startPositionY;
+
+		nodes = new Node[numNodesInElevator];
+		left = new Door ();
+		right = new Door ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
